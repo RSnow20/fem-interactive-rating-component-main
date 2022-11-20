d90b4a9ea377c73458888d7464e0bd147f45daa1
Ryan <116980194+RSnow20@users.noreply.github.com>
2022-11-20 13:02:55 -0500

    Completed the "Thank you!" card. Added JS to rerender the card wrapper to veiw submitted rating

--- main.js
+++ main.js
@@ -9,6 +9,12 @@ function init() {
       setActiveRating(button);
     });
   });
+
+  let buttonSubmit = document.querySelector("#rating-submit");
+
+  buttonSubmit.addEventListener("click", function () {
+    submit(ratingActive);
+  });
 }
 
 function setActiveRating(ratingButton) {
@@ -19,16 +25,65 @@ function setActiveRating(ratingButton) {
   ratingActive = ratingButton.getAttribute("id");
   ratingButton.classList.add("rating-active");
 
-  let ratingButtons = document.querySelectorAll(".rating-star");
+  let ratingStars = document.querySelectorAll(".rating-star");
 
-  ratingButtons.forEach((button) => {
-    let buttonID = button.getAttribute("id");
-    if (buttonID != ratingButton.getAttribute("id")) {
-      button.classList.remove("rating-active");
+  ratingStars.forEach((star) => {
+    let starID = star.getAttribute("id");
+    if (starID != ratingButton.getAttribute("id")) {
+      star.classList.remove("rating-active");
     }
   });
 }
 
-init();
+function submit(rating) {
+  if (!rating) {
+    return;
+  }
 
-console.log(ratingActive);
+  renderThankYou(rating);
+}
+
+function renderThankYou(rating) {
+  if (!rating) {
+    return;
+  }
+
+  let cardWrapper = document.querySelector(".rating-card-wrapper");
+
+  cardWrapper.innerHTML = "";
+  cardWrapper.style.justifyContent = "center";
+
+  let svg = document.createElement("img");
+  svg.src = "images/illustration-thank-you.svg";
+  svg.alt = "Phone with review being submitted";
+  svg.style.marginInline = "auto";
+  svg.style.marginTop = "1rem";
+  svg.style.marginBottom = "2rem";
+  cardWrapper.appendChild(svg);
+
+  let ratingDiv = document.createElement("div");
+
+  ratingDiv.innerHTML = "You selected " + rating + " out of 5";
+  ratingDiv.classList.add("submitted-rating");
+
+  cardWrapper.appendChild(ratingDiv);
+
+  let title = document.createElement("h1");
+
+  title.innerHTML = "Thank you!";
+  title.classList.add("rating-title");
+  title.style.textAlign = "center";
+
+  cardWrapper.appendChild(title);
+
+  let description = document.createElement("p");
+
+  description.innerHTML =
+    "We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!";
+  description.classList.add("rating-description");
+  description.style.textAlign = "center";
+
+  cardWrapper.appendChild(description);
+}
+
+init();
